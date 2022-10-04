031b65d9bb94586b580b4bfa113b87cbc06c0d82
arvene <arvene241@gmail.com>
2022-10-04 14:07:35 +0800

    fix issue

--- src/components/ProductImagesMobile.jsx
+++ src/components/ProductImagesMobile.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import BtnSlider from "../../../ecommerce/src/components/BtnSlider";
+import BtnSlider from "./BtnSlider";
 
 const Container = styled.div`
   width: 100%;
